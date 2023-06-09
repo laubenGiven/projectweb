4e6994e642afe28e3e1c5c9059bddd5a8bc07421
laubenGiven <mpairwelauben375@gmail.com>
2023-06-09 22:41:24 +0300

    updated and restructured whole project

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const mysql = require('mysql');
+const mongoose = require('mongoose');
 const express = require('express');
 const session = require('express-session');
+require('dotenv').config();
+const PORT = process.env.PORT
+const mongoconn = process.env.MongodbConn
 const path = require('path');
 const { Console } = require('console');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+const {Admin,createDefaultUser} = require('./models/Admin');
+
+const loginrouter = require('./routes/login')
 const app = express();
+
+
+
+
+
+
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/login',loginrouter)
+
 
 
 // Set EJS as the template engine
@@ -16,20 +32,16 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Set the views directory path
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'lee',
-    password: 'alien123.com',
-    database: 'nodelogin'
-});
 
-// testing database connection
-connection.connect(function(error){
-	if(error) throw error
-	else  console.log('************************Successful Database connection************************');
-});
-
-
+// Connect to the MongoDB database
+mongoose.connect(mongoconn, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+  });
+  // middle ware configuration
 app.use(session({
 	secret: 'secret',
 	resave: true,
@@ -46,7 +58,7 @@ app.get('/', function(request, response) {
 // Serve the lregister  page
 
 app.get('/newproperty', function(request, response) {
-	response.sendFile(path.join(__dirname + '/public/newproperty.html'));
+	response.sendFile(path.join(__dirname + '/newproperty.html'));
 });
 
 
@@ -57,14 +69,15 @@ app.get('/newproperty', function(request, response) {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create a MySQL connection pool
-const pool = mysql.createPool({
+/*
+const pool = mongoose.createPool({
   connectionLimit: 10,
   host: 'localhost',
   user: 'lee',
   password: 'alien123.com',
   database: 'nodelogin',
 });
-
+*/
 
 
 // Handle POST requests to the registration form
@@ -86,7 +99,7 @@ app.post('/register', (req, res) => {
 });
 
 
-
+/*
 // Handle POST requests to the New property form
 app.post('/newproperty', (req, res) => {
   // Extract form data from request body
@@ -162,176 +175,9 @@ app.post('/auth', function(request, response) {
 });
 
 
-
-
-// Server
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-});
-
-
-
-
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
-// Route for resetting password
-app.post('/resetpassword', (req, res) => {
-  const email = req.body.email;
-
-  // Query database for user with provided email address
-  connection.query('SELECT * FROM users WHERE email = ?', email, (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    } else if (results.length === 0) {
-      res.send('No user found with that email address');
-    } else {
-      const user = results[0];
-      const newPassword = generateRandomPassword();
-      
-      // Update user's password in database
-      connection.query('UPDATE users SET password = ? WHERE id = ?', [newPassword, user.id], (error, results) => {
-        if (error) {
-          console.error(error);
-          res.status(500).send('Internal Server Error');
-        } else {
-          // Send email with new password
-          const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: 'your_email@gmail.com',
-              pass: 'your_email_password'
-            }
-          });
-
-          const mailOptions = {
-            from: 'your_email@gmail.com',
-            to: user.email,
-            subject: 'Password Reset',
-            text: `Your new password is: ${newPassword}`
-          };
-
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.error(error);
-              res.status(500).send('Internal Server Error');
-            } else {
-              console.log('Email sent: ' + info.response);
-              res.redirect('/password_reset_confirmation');
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
-// Route for confirmation page
-app.get('/password_reset_confirmation', (req, res) => {
-  res.send('Your password has been reset. Please check your email for your new password.');
-});
-
-
-
-
- * 
- * 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- const express = require('express');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-
-
-const app = express();
-app.use(express.static('public'));
-
-// Database connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'lee',
-  password: ' alien123.com',
-  database: 'nodelogin'
-});
-// testing database connection
-connection.connect(function(error){
-	if(error) throw error
-	else  console.log('************************Successful Database connection************************');
-});
-
-// Express session
-app.use(session({
-  secret: 'secret-key',
-  resave: true,
-  saveUninitialized: true
-}));
-
-// Login route
-app.post('/auth', (req, res) => {
-  const { username, password } = req.body;
-
-  // Check if the user exists in the database
-  db.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
-    if (error) {
-      res.status(500).send('Internal Server Error');
-    } else if (results.length === 0) {
-      res.status(401).send('Invalid username or password');
-    } else {
-      // Check if the password matches
-      bcrypt.compare(password, results[0].password, (err, isMatch) => {
-        if (err) {
-          res.status(500).send('Internal Server Error');
-        } else if (!isMatch) {
-          res.status(401).send('Invalid username or password');
-        } else {
-          // Set the user session
-          req.session.user = username;
-          res.redirect('/home');
-        }
-      });
-    }
-  });
-});
-
-// Dashboard route
-app.get('/home', (req, res) => {
-  // Check if the user is logged in
-  if (!req.session.user) {
-    res.redirect('/login');
-  } else {
-    res.send(`Welcome ${req.session.user}`);
-  }
-});
-
-// Server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
 */
+
+// Server
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+})
